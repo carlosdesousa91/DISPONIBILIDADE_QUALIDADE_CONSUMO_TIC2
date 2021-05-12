f2a0567dcd18861ba02a927c8bbddd095e9c7272
Carlos Araujo de Sousa <carlos.sousa@terceiro.rnp.br>
2021-05-12 20:18:57 -0300

    gerar token de acesso

--- powerbi.php
+++ powerbi.php
@@ -91,7 +91,7 @@ function embeddedToken($access_token_decoded){
 
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 
-        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_CUSTOMREQUEST => "POST",
 
         //CURLOPT_POSTFIELDS => $body,
 
