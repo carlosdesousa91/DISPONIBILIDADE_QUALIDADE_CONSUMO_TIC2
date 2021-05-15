4238c31b733cab0e8b191869ba2b80c157a5d464
Carlos Araujo de Sousa <carlos.sousa@terceiro.rnp.br>
2021-05-15 14:00:02 -0300

    testar access levels

--- relatorio_backbone_quedas_circuitos.php
+++ relatorio_backbone_quedas_circuitos.php
@@ -45,7 +45,7 @@ var embedConfiguration = {
 	accessToken: "<?php echo $embedded_token; ?>",
 	settings: {
 		filterPaneEnabled: true,
-		navContentPaneEnabled: false,
+		navContentPaneEnabled: true,
 		pageName:true
 	}
 
