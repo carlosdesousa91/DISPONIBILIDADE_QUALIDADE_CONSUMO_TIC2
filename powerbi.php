09d8f96b4259d19704d187afaed78492b82638bd
Carlos Araujo de Sousa <carlos.sousa@terceiro.rnp.br>
2021-05-15 13:32:10 -0300

    testar access levels

--- powerbi.php
+++ powerbi.php
@@ -122,7 +122,7 @@ function embeddedToken($access_token_decoded){
 		return 1;
 	}
     $argument = array(        
-        'accessLeval' => 'edit',
+        'accessLevel' => 'edit',
         'request' => 'Post'      
     );
     $argument_json = json_encode($argument);
